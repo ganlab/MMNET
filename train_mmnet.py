6379df7b93ffd858daf38986cb166d86ab08f7a7
Shengjie Ren <1302976472@qq.com>
2025-05-12 16:01:52 +0800

    Update
    Update

--- train_mmnet.py
+++ train_mmnet.py
@@ -20,7 +20,7 @@ torch.backends.cudnn.deterministic = True
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="MMNet")
-    parser.add_argument("--phenotype", type=str, default="grainlength", help="The name of the phenotype")
+    parser.add_argument("--phenotype", type=str, default="default", help="The name of the phenotype")
     parser.add_argument("--windows_mechanism", type=int, default=0,
                         help="Windows mechanism 0: don't use windows_mechanism, 1: windows_mechanism by chromosome")
     parser.add_argument("--windows_chr", type=int, default=2, help="the number of chromosomes in a window")
